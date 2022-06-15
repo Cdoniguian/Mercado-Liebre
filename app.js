eab69d0b5535c73c5344a2b14663282f4e7e2749
Cdoniguian <christian_doniguian@yahoo.com>
2022-06-15 17:10:34 -0300

    inicializacion de repositorio

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// configuracón del servidor
+ app.use(express.static('public'));
+
+app.get('/', (req,res)=>{res.sendFile(path.join(__dirname , './views/home.html'))})
+
+app.get('/home', (req,res)=>{res.sendFile(path.join(__dirname , './views/home.html'))})
+
+app.get('/register', (req,res)=>{res.sendFile(path.join(__dirname , './views/register.html'))}) 
+
+app.get('/login', (req,res)=>{res.sendFile(path.join(__dirname , './views/login.html'))}) 
+
+app.listen(3030, ()=>{console.log('servidor corriendo correctamente')});
